ba737483df6bd15fb17c888b47e97cb8f8856471
Aryan <Aryans8791@gmail.com>
2022-08-26 16:41:22 +0530

    css convert

--- client/src/Convert.js
+++ client/src/Convert.js
@@ -11,18 +11,22 @@ const [simplinews, setSimpliNews] = useState()
         setNews(e.target.value)
     }
 let textFromFile = " "
-function loadFileAsText(){
+function loadFileAsText(e){
     var fileToLoad = document.getElementById("input-file").files[0]
     console.log(fileToLoad)
+    e.preventDefault();
     var fileReader = new FileReader();
     fileReader.onload = function(fileLoadedEvent){
         var textFromFileLoaded = fileLoadedEvent.target.result;
-        textFromFile =textFromFileLoaded ;
-        console.log(textFromFile);
-        document.getElementById("output").value = textFromFileLoaded;
+        // console.log(fileLoadedEvent)
+        console.log(textFromFileLoaded)
+        // textFromFile =textFromFileLoaded ;
+        // console.log("This is Textfile"+Object.values(textFromFile));
+        // document.getElementById("output").value = textFromFileLoaded;
+        // document.getElementById("inputting-file")
     };
-  
     fileReader.readAsText(fileToLoad, "UTF-8");
+    
     setNews(textFromFile)
     handleSumarize()
   }
@@ -46,10 +50,11 @@ const handleSumarize = async()=>{
 }
     console.log(news)
   return (
+        <div className='convertout'>
         <div className='convert'>
             <div className='uploadbtn'>
             <input type="file" id="input-file"></input>
-            <Button variant="contained" onClick={loadFileAsText}>Upload .txt</Button> 
+            <Button variant="contained" onClick={loadFileAsText}  style={{maxWidth: '100px', maxHeight: '30px', minWidth: '100px', minHeight: '30px', fontSize:10}}>Upload .txt</Button> 
             <input id="output" value={textFromFile}></input>
             </div>
             <div className='uploadingbtn'>
@@ -58,15 +63,21 @@ const handleSumarize = async()=>{
             </div>
             <div className='conversionform'>
             <form className='formconvert' onSubmit={(e)=>{e.preventDefault()}}>
-                <textarea rows='20' cols='40' onChange={handleSubmit}></textarea>
+                <textarea className="sendingArea" id="inputting-file" rows='20' cols='40' onChange={handleSubmit}></textarea>
                 {/* <button onClick={handleSumarize}>Sumarize and Simplify</button> */}
-                <Button variant="contained" onClick={handleSumarize} >Summarize and Simplify</Button>
+                <Button variant="contained" onClick={handleSumarize} style={{margin: "10px"}}>Summarize and Simplify</Button>
                 {/* <button>Simplify</button> */}
             </form>
-            <textarea rows='20' cols='40' value={simnews}></textarea>
-            <textarea rows='20' cols='40' value={simplinews}></textarea>
-            {/* <input type="text" ></input> */}
+            <div className='simplified'>
+            <textarea className="textingArea" rows='20' cols='40' value={simnews}></textarea>
+            <h3 className='newstext'>Summarized</h3>
             </div>
+            <div className='simplified'>
+            <textarea className="textingArea" rows='20' cols='40' value={simplinews}></textarea>
+            <h3 className='newstext'>Simplified</h3>
+            </div>
+            </div>
+        </div>
         </div>
   )
 }
